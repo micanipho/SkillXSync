2350274f523d9ca7b20594fddc0ccac01331fcf3
nhlakanipho <nqmasilela777@gmail.com>
2025-03-04 23:56:11 +0200

    initial commit

---
+++ src/main/java/org/skillxsync/SkillXSyncApplication.java
@@ -0,0 +1,13 @@
+package org.skillxsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillXSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillXSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skillxsync/SkillXSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skillxsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillXSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
